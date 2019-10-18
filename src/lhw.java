b948f87c7d4929f2563a08b311b12e0f1e243bcb
Kamila Dmitrieva <dmtrieva_kamila@mail.com>
2019-10-18 23:06:03 +0300

    Initial commit

---
+++ src/lhw.java
@@ -0,0 +1,41 @@
+public class lhw {
+    public static void main(String[] args) {
+        int n = 10;
+        int[] mas = new int[n];
+        for (int i = 0; i < n; i++) {
+            mas[i] = i * i;
+            System.out.println(mas[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        double[][] mas = new double[10][];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = new double[10];
+        }
+
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                System.out.println("" + i + "," + j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        long k = 10;
+        long[] mas = new long[k];
+        for (long i = 0; i < k; i++) {
+            mas[i] = i * i;
+            System.out.println(mas[i]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        boolean[] boolArr = new boolean[true; false];
+        boolean[] b = new boolean[2];
+        b[0] = true;
+        b[1] = false;
+        System.out.println(boolArr[b])
+    }
+}
